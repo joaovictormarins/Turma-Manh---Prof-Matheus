8af5015dc2a938872efdf6cc06921a7b0929253d
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-05-01 10:40:17 -0300

    Atualizar o Calculadora de Desconto.py

--- Calculadora de Desconto.py
+++ Calculadora de Desconto.py
@@ -1,3 +1,3 @@
-preco = float(input('Qual é  preço do produto? R$'))
+preco = float(input('Qual é  preço inicial do produto? R$'))
 novo = preco - (preco * 5 / 100)
 print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${}'.format(preco,novo))
