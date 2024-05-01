47b2c946d6ee73ca742bea5e5fc3ef41a62b2efd
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-05-01 10:38:43 -0300

    Atualizar o #Calculadora de Juros Simples.py

--- #Calculadora de Juros Simples.py
+++ #Calculadora de Juros Simples.py
@@ -1,6 +1,6 @@
 #Calculadora de Juros Simples
-c = float(input('capital inicial'))
-i = float(input('taxa de juros'))
+c = float(input('Quantia inicial'))
+i = float(input('juros'))
 t = float(input("tempo:"))
 J = (c * i * t) 
 print(J)
