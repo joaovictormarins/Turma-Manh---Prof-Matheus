45fe54160b39f05853e1b63a0a2b7a104b2a2ec9
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-05-02 08:23:31 -0300

    Update Verificação  de triangulo.py

--- Vereificaçao de triangulo.py
+++ Vereificaçao de triangulo.py
@@ -10,4 +10,4 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     else:
         print('Isósceles!')
 else: 
-    print('Os segmentos acima não podem formar um triangulo ')
+    print('Os segmentos acima não podem formar um triangulo através dos critérios estabelecidos pelo usuário')
