cfa2f67a6385feb6fe77023b36aa1efeb2998ad6
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-05-01 22:46:15 -0300

    Atualizar o Verificador de ano Bissexto.py

--- Verificador de ano Bissexto.py
+++ Verificador de ano Bissexto.py
@@ -1,8 +1,6 @@
-from datetime import date 
-ano = int(input('Que ano quer analisar? coloque 0 para analisar o ano atual:'))
-if ano == 0:
-    ano = date.today().year
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print('O ano {} é bissexto'.format(ano))
+ano = int(input("Digite um ano: "))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(ano, "é um ano bissexto.")
 else:
-    print('O ano {} não é bissexto'.format(ano))
+    print(ano, "não é um ano bissexto.")
