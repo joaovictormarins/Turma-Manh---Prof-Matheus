0b3e832150a70f74bff3dcf32f37420853ae2c19
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-05-01 22:32:58 -0300

    Atualizar o Conversor de nota em conceito.py

--- Conversor de nota em conceito.py
+++ Conversor de nota em conceito.py
@@ -1,20 +1,14 @@
-opcao = int(input('Inserir dados? 0 - Não   1 - Sim'))
-while opcao == 1:
-    aluno = input('Nome do aluno: ')
-    nota = float(input('Nota final: '))
+nota = float(input("Digite a nota do aluno: "))
 
-    if 0 <= nota < 3:
-        conceito = 'E'
-    elif 3 <= nota < 5:
-        conceito = 'D'
-    elif 5 <= nota < 7:
-        conceito = 'C'
-    elif 7 <= nota <= 8.9:
-        conceito = 'B'
-    elif 9 <= nota <= 10:
-        conceito = 'A'
-    else:
-        print('Nota inválida, encerrando o programa.')
-        exit()
-    print(f'O aluno {aluno} tirou a nota {nota} e se enquadra no conceito {conceito}')
-    opcao = int(input('Inserir dados? 0 - Não  1 - Sim '))
+if nota >= 9:
+    conceito = 'A'
+elif nota >= 7.5:
+    conceito = 'B'
+elif nota >= 6:
+    conceito = 'C'
+elif nota >= 4:
+    conceito = 'D'
+else:
+    conceito = 'F'
+
+print("Conceito correspondente:", conceito)
