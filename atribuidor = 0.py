fa8644e12408fe6611d4028dea48c454b407de81
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-06-05 08:41:32 -0300

    Add files via upload

---
+++ atribuidor = 0.py
@@ -0,0 +1,8 @@
+atribuidor = 0 
+contador = 0 
+
+while contador < 10:
+    numero = int(input("Digite um número inteiro:"))
+    atribuidor += numero 
+    contador += 1 
+print("A soma dos 10 números inteiros é:", atribuidor)
