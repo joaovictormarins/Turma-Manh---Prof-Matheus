92e5ad51b40770a68a5739ca73b0050efa6b7cda
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-06-06 08:25:29 -0300

    Update atribuidor= 0.py

--- atribuidor= 0.py
+++ atribuidor= 0.py
@@ -5,4 +5,4 @@ while contador < 10:
     numero = int(input("Digite um número inteiro:"))
     atribuidor += numero 
     contador += 1 
-print("A soma dos 10 números inteiros é:", atribuidor)
+print("A atribuicao dos 10 números inteiros é:", atribuidor)
