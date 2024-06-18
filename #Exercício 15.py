909c330fc506c93cc3cae9c5c082dec59a89ec84
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-06-18 10:24:29 -0300

    Add files via upload

---
+++ #Exercício 15.py
@@ -0,0 +1,29 @@
+#Exercício 15 
+comidas = ["leite","couve","computador","tomate","garfo","faca","tablet","refrigerante"]
+bebidas = ["uva","colher","tv","vinho","cerveja","celular","banana"]
+talheres = ["arroz","iphone","concha","whisky","vodka","feijão","colher de chá"]
+eletronicos = []
+
+comidas.remove('garfo')
+comidas.remove("computador")
+comidas.remove("tablet")
+comidas.remove('faca')
+comidas.remove('refrigerante')
+comidas.extend(['banana', 'uva', 'arroz', 'feijao'])
+
+bebidas.remove("colher")
+bebidas.remove("tv")
+bebidas.remove("celular")
+bebidas.remove("banana")
+bebidas.remove("uva")
+bebidas.extend(["leite","refrigerante","whisky","vodka"])
+
+talheres.remove("arroz")
+talheres.remove("iphone")
+talheres.remove("whisky")
+talheres.remove("vodka")
+talheres.remove("feijão")
+talheres.extend(["colher","garfo","faca"])
+
+eletronicos.extend(["computador","tablet","tv","celular","iphone"])
+
