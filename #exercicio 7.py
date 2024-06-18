6d7e3251840248fc3840ef848ba42283a98eba66
joaovictormarins <166523898+joaovictormarins@users.noreply.github.com>
2024-06-18 10:55:25 -0300

    Add files via upload

---
+++ #exercicio 7.py
@@ -0,0 +1,10 @@
+#exercicio 7 
+
+lista = []
+
+while True:
+    itens = str(input("Digite os elementos que desejar(Digite: pare , quando terminar:  "))
+    if itens == "pare":
+        break 
+    lista.append(itens)
+print(lista)
